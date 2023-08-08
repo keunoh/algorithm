87132786f4457d1b4c118f6e810b60868faca2b5
unknown <rmsdhcjswo1@gmail.com>
2023-08-08 17:59:56 +0900

    time flexability

---
+++ src/Back24262.java
@@ -0,0 +1,8 @@
+public class Back24262 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+    // 주어진 알고리즘은 입력값에 관계없이 1번만 수행되는 알고리즘
+    // 수행시간은 상수시간?
+}
